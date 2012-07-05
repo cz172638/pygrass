d31a24257e48d48a3eabecdac6934f76b6b9b96f
pietro <pietro.zambelli@ing.unitn.it>
2012-07-05 16:14:48 +0200

    Forgot changes in the __init__ file, to add the History class

--- pygrass/__init__.py
+++ pygrass/__init__.py
@@ -5,9 +5,12 @@ Created on Fri May 25 12:55:14 2012
 @author: pietro
 """
 
+import grass.lib.gis as libgis
+libgis.G_gisinit('')
 from buffer import Buffer
 from segment import Segment
 from rowio import RowIO
 from category import Category
+from history import History
 from raster import RasterRow, RasterSegment, RasterRowIO, RasterNumpy
 from region import Region
