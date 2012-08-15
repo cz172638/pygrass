06e3e2031d0094297c247709262348ae6f03bf96
pietro <pietro.zambelli@ing.unitn.it>
2012-08-15 17:57:30 +0200

    Split the Vector class into: Vector (without topology) and VectTopo (with topology), add a new errors file containig all the exceptions

--- pygrass/vector/__init__.py
+++ pygrass/vector/__init__.py
@@ -8,33 +8,16 @@ import ctypes
 import grass.lib.vector as libvect
 from vector_type import VTYPE, GV_TYPE
 import geometry as geo
-from basic import Bbox
-from table import DBlinks
-
 import sys
 import os
-
+# modify the python path to load "env" and "errors"
 vectorpath = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(vectorpath)
 sys.path.append("%s/.." % vectorpath)
 import env
-
-
-class GrassError(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
-
-
-class OpenError(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
-
+from errors import GrassError, OpenError
+from basic import Bbox
+from table import DBlinks
 
 _NUMOF = {"areas": libvect.Vect_get_num_areas,
           "dblinks": libvect.Vect_get_num_dblinks,
@@ -60,11 +43,6 @@ _GEOOBJ = {"areas": geo.Area,
            "nodes": geo.Node,
            "volumes": None}
 
-                 # 0    1         2       3           4           5
-feature_label = [None, 'point', 'line', 'boundary', 'centroid', 'face',
-                 # 6        7        8
-                 'kernel', 'area', 'volume']
-
 
 #=============================================
 # VECTOR
@@ -75,7 +53,7 @@ class Vector(object):
 
         >>> from pygrass.vector import Vector
         >>> municip = Vector('boundary_municp_sqlite')
-        >>> municip.isopen()
+        >>> municip.is_open()
         False
         >>> municip.mapset
         ''
@@ -83,8 +61,6 @@ class Vector(object):
         True
         >>> municip.mapset
         'user1'
-        >>> municip.topology
-        False
         >>> municip.overwrite
         False
 
@@ -97,9 +73,16 @@ class Vector(object):
         self.mapset = mapset
         self.c_mapinfo = ctypes.pointer(libvect.Map_info())
         self._spatial_index = libvect.spatial_index()
-        self.topology = False
+        self._topo_level = 1
+        self._class_name = 'Vector'
         self.overwrite = False
         self.dblinks = None
+
+    def __repr__(self):
+        if self.exist():
+            return "%s(%r, %r)" % (self._class_name, self.name, self.mapset)
+        else:
+            return "%s(%r)" % (self._class_name, self.name)
 
     def __iter__(self):
         """::
@@ -108,7 +91,7 @@ class Vector(object):
             >>> mun.open()
             >>> features = [feature for feature in mun]
             >>> features[:3]
-            [Boundary(v_id=1), Boundary(v_id=2), Boundary(v_id=3)]
+            [Boundary(v_id=None), Boundary(v_id=None), Boundary(v_id=None)]
             >>> mun.close()
 
         ..
@@ -116,13 +99,151 @@ class Vector(object):
         #return (self.read(f_id) for f_id in xrange(self.num_of_features()))
         return self
 
+    def next(self):
+        """::
+
+            >>> mun = Vector('boundary_municp_sqlite')
+            >>> mun.open()
+            >>> mun.next()
+            Boundary(v_id=None)
+            >>> mun.next()
+            Boundary(v_id=None)
+            >>> mun.close()
+
+        ..
+        """
+        v_id = self.c_mapinfo.contents.next_line
+        v_id = v_id if v_id != 0 else None
+        c_points = ctypes.pointer(libvect.line_pnts())
+        c_cats = ctypes.pointer(libvect.line_cats())
+        ftype = libvect.Vect_read_next_line(self.c_mapinfo, c_points, c_cats)
+        if ftype == -2:
+            raise StopIteration()
+        if ftype == -1:
+            raise
+        #if  GV_TYPE[ftype]['obj'] is not None:
+        return GV_TYPE[ftype]['obj'](v_id=v_id,
+                                     c_mapinfo=self.c_mapinfo,
+                                     c_points=c_points,
+                                     c_cats=c_cats)
+
+    def exist(self):
+        if self.name:
+            self.mapset = env.get_mapset_vector(self.name, self.mapset)
+        else:
+            return False
+        if self.mapset:
+            return True
+        else:
+            return False
+
+    def is_open(self):
+        return bool(self.c_mapinfo.contents.open)
+
+    def open(self, mode='r', layer='0', overwrite=None):
+        """::
+
+            >>> mun = Vector('boundary_municp_sqlite')
+            >>> mun.open()
+            >>> mun.is_open()
+            True
+            >>> mun.close()
+
+        ..
+        """
+        if libvect.Vect_set_open_level(self._topo_level) != 0:
+            raise OpenError("Invalid access level.")
+        # update the overwrite attribute
+        self.overwrite = overwrite if overwrite is not None else self.overwrite
+        # check if the mode is valid
+        if mode not in ('r', 'rw', 'w'):
+            raise ValueError("Mode not supported. Use one of: 'r', 'rw', 'w'.")
+        # check if the map exist
+        if self.exist() and mode == 'r':
+            openvect = libvect.Vect_open_old2(self.c_mapinfo, self.name,
+                                              self.mapset, layer)
+        # If it is opened in write mode
+        if mode == 'w':
+            openvect = libvect.Vect_open_new(self.c_mapinfo, self.name,
+                                             libvect.WITHOUT_Z)
+        elif mode == 'rw':
+            openvect = libvect.Vect_open_update2(self.c_mapinfo, self.name,
+                                                 self.mapset, layer)
+        # initialize the dblinks object
+        self.dblinks = DBlinks(self.c_mapinfo)
+        # check the C function result.
+        if openvect != self._topo_level:
+            str_err = "Not able to open the map, C function return %d."
+            raise OpenError(str_err % openvect)
+
+    def close(self):
+        if self.is_open():
+            if libvect.Vect_close(self.c_mapinfo) != 0:
+                str_err = 'Error when trying to close the map with Vect_close'
+                raise GrassError(str_err)
+
+    def bbox(self):
+        """Return the BBox of the vecor map
+        """
+        bbox = Bbox()
+        libvect.Vect_get_map_box(self.c_mapinfo, bbox.c_bbox)
+        return bbox
+
+    def write(self, geo_obj):
+        """::
+
+            >>> mun = Vector('boundary_municp_sqlite')         #doctest: +SKIP
+            >>> mun.open(mode='rw')            #doctest: +SKIP
+            >>> feature1 = mun.read(1)                         #doctest: +SKIP
+            >>> feature1                                       #doctest: +SKIP
+            Boundary(v_id=1)
+            >>> feature1[:3]             #doctest: +SKIP +NORMALIZE_WHITESPACE
+            [Point(463718.874987, 310970.844494),
+             Point(463707.405987, 310989.499494),
+             Point(463714.593986, 311084.281494)]
+            >>> from geometry import Point                     #doctest: +SKIP
+            >>> feature1.insert(1, Point(463713.000000, 310980.000000)) #doctest: +SKIP
+            >>> feature1[:4]                   #doctest: +SKIP +NORMALIZE_WHITESPACE
+            [Point(463718.874987, 310970.844494),
+             Point(463713.000000, 310980.000000),
+             Point(463707.405987, 310989.499494),
+             Point(463714.593986, 311084.281494)]
+            >>> mun.write(feature1)                            #doctest: +SKIP
+            >>> feature1                                       #doctest: +SKIP
+            Boundary(v_id=8708)
+            >>> mun.close()                                    #doctest: +SKIP
+
+        ..
+        """
+        result = libvect.Vect_write_line(self.c_mapinfo, geo_obj.gtype,
+                                         geo_obj.c_points, geo_obj.c_cats)
+        if result == -1:
+            raise GrassError("Not able to write the vector feature.")
+        if self._topo_level == 2:
+            # return new feature id (on level 2)
+            geo_obj.id = result
+        else:
+            # return offset into file where the feature starts (on level 1)
+            geo_obj.offset = result
+
+
+#=============================================
+# VECTOR WITH TOPOLOGY
+#=============================================
+
+class VectTopo(Vector):
+    def __init__(self, *args, **kargs):
+        super(VectTopo, self).__init__(*args, **kargs)
+        self._topo_level = 2
+        self._class_name = 'VectTopo'
+
     def __len__(self):
         return libvect.Vect_get_num_lines(self.c_mapinfo)
 
     def __getitem__(self, key):
         """::
 
-            >>> mun = Vector('boundary_municp_sqlite')
+            >>> mun = VectTopo('boundary_municp_sqlite')
             >>> mun.open()
             >>> mun[:3]
             [Boundary(v_id=1), Boundary(v_id=2), Boundary(v_id=3)]
@@ -140,42 +261,6 @@ class Vector(object):
         else:
             raise ValueError("Invalid argument type: %r." % key)
 
-    def __repr__(self):
-        if self.exist():
-            return "Vector(%r, %r)" % (self.name, self.mapset)
-        else:
-            return "Vector(%r)" % self.name
-
-    def __del__(self):
-        self.close()
-
-    def next(self):
-        """::
-
-            >>> mun = Vector('boundary_municp_sqlite')
-            >>> mun.open()
-            >>> mun.next()
-            Boundary(v_id=1)
-            >>> mun.next()
-            Boundary(v_id=2)
-            >>> mun.close()
-
-        ..
-        """
-        v_id = self.c_mapinfo.contents.next_line
-        c_points = ctypes.pointer(libvect.line_pnts())
-        c_cats = ctypes.pointer(libvect.line_cats())
-        ftype = libvect.Vect_read_next_line(self.c_mapinfo, c_points, c_cats)
-        if ftype == -2:
-            raise StopIteration()
-        if ftype == -1:
-            raise
-        #if  GV_TYPE[ftype]['obj'] is not None:
-        return GV_TYPE[ftype]['obj'](v_id=v_id,
-                                     c_mapinfo=self.c_mapinfo,
-                                     c_points=c_points,
-                                     c_cats=c_cats)
-
     def num_primitive_of(self, primitive):
         """primitive are:
             * "boundary",
@@ -185,12 +270,12 @@ class Vector(object):
             * "line",
             * "point" ::
 
-            >>> rail = Vector('rail')
+            >>> rail = VectTopo('rail')
             >>> rail.open()
             >>> rail.num_primitive_of('line')
             10837
-            >>> municip = Vector('boundary_municp_sqlite')
-            >>> municip.open(topology=True)
+            >>> municip = VectTopo('boundary_municp_sqlite')
+            >>> municip.open()
             >>> municip.num_primitive_of('line')
             0
             >>> municip.num_primitive_of('centroid')
@@ -211,8 +296,8 @@ class Vector(object):
                   "line_points", "lines", "nodes", "update_lines",
                   "update_nodes", "volumes"]
 
-            >>> municip = Vector('boundary_municp_sqlite')
-            >>> municip.open(topology=True)
+            >>> municip = VectTopo('boundary_municp_sqlite')
+            >>> municip.open()
             >>> municip.number_of("areas")
             3579
             >>> municip.number_of("islands")
@@ -246,8 +331,8 @@ class Vector(object):
         """Return an iterator of vector features
 
         ::
-            >>> municip = Vector('boundary_municp_sqlite')
-            >>> municip.open(topology=True)
+            >>> municip = VectTopo('boundary_municp_sqlite')
+            >>> municip.open()
             >>> big = [area for area in municip.viter('areas')
             ...        if area.alive() and area.area >= 10000]
             >>> big[:3]
@@ -263,7 +348,7 @@ class Vector(object):
             Area(3102) 697521857.848
             Area(2682) 320224369.66
             Area(2552) 298356117.948
-            >>> munico.close()
+            >>> municip.close()
 
         ..
         """
@@ -278,7 +363,7 @@ class Vector(object):
     def rewind(self):
         """Rewind vector map to cause reads to start at beginning. ::
 
-            >>> mun = Vector('boundary_municp_sqlite')
+            >>> mun = VectTopo('boundary_municp_sqlite')
             >>> mun.open()
             >>> mun.next()
             Boundary(v_id=1)
@@ -298,7 +383,7 @@ class Vector(object):
     def read(self, feature_id):
         """Return a geometry object given the feature id. ::
 
-            >>> mun = Vector('boundary_municp_sqlite')
+            >>> mun = VectTopo('boundary_municp_sqlite')
             >>> mun.open()
             >>> feature1 = mun.read(0)                     #doctest: +ELLIPSIS
             Traceback (most recent call last):
@@ -340,53 +425,13 @@ class Vector(object):
         else:
             raise ValueError('The index must be >0, %r given.' % feature_id)
 
-    def write(self, geo_obj):
-        """::
-
-            >>> mun = Vector('boundary_municp_sqlite')         #doctest: +SKIP
-            >>> mun.open(mode='rw', topology=False)            #doctest: +SKIP
-            >>> feature1 = mun.read(1)                         #doctest: +SKIP
-            >>> feature1                                       #doctest: +SKIP
-            Boundary(v_id=1)
-            >>> feature1[:3]             #doctest: +SKIP +NORMALIZE_WHITESPACE
-            [Point(463718.874987, 310970.844494),
-             Point(463707.405987, 310989.499494),
-             Point(463714.593986, 311084.281494)]
-            >>> from geometry import Point                     #doctest: +SKIP
-            >>> feature1.insert(1, Point(463713.000000, 310980.000000)) #doctest: +SKIP
-            >>> feature1[:4]                   #doctest: +SKIP +NORMALIZE_WHITESPACE
-            [Point(463718.874987, 310970.844494),
-             Point(463713.000000, 310980.000000),
-             Point(463707.405987, 310989.499494),
-             Point(463714.593986, 311084.281494)]
-            >>> mun.write(feature1)                            #doctest: +SKIP
-            >>> feature1                                       #doctest: +SKIP
-            Boundary(v_id=8708)
-            >>> mun.close()
-
-        ..
-        """
-        result = libvect.Vect_write_line(self.c_mapinfo, geo_obj.gtype,
-                                         geo_obj.c_points, geo_obj.c_cats)
-        if result == -1:
-            raise GrassError("Not able to write the vector feature.")
-        if self.topology:
-            # return new feature id (on level 2)
-            geo_obj.id = result
-        else:
-            # return offset into file where the feature starts (on level 1)
-            geo_obj.offset = result
-
     def rewrite(self, geo_obj):
-        if self.topology:
-            result = libvect.Vect_rewrite_line(self.c_mapinfo,
-                                               geo_obj.id, geo_obj.gtype,
-                                               geo_obj.c_points,
-                                               geo_obj.c_cats)
-            # return offset into file where the feature starts
-            geo_obj.offset = result
-        else:
-            raise OpenError("the vector map must be open with topology.")
+        result = libvect.Vect_rewrite_line(self.c_mapinfo,
+                                           geo_obj.id, geo_obj.gtype,
+                                           geo_obj.c_points,
+                                           geo_obj.c_cats)
+        # return offset into file where the feature starts
+        geo_obj.offset = result
 
     def delete(self, feature_id):
         if libvect.Vect_rewrite_line(self.c_mapinfo, feature_id) == -1:
@@ -399,74 +444,3 @@ class Vector(object):
                 raise GrassError("C funtion: Vect_restore_line.")
         else:
             raise ValueError("The value have not an offset attribute.")
-
-    def exist(self):
-        if self.name:
-            self.mapset = env.get_mapset_vector(self.name, self.mapset)
-        else:
-            return False
-        if self.mapset:
-            return True
-        else:
-            return False
-
-    def isopen(self):
-        return bool(self.c_mapinfo.contents.open)
-
-    def open(self, mode='r', layer='0', topology=None, overwrite=None):
-        """::
-
-            >>> mun = Vector('boundary_municp_sqlite')
-            >>> mun.open()
-            >>> mun.topology
-            False
-            >>> mun.is_open()
-            True
-            >>> mun.close()
-
-        ..
-        """
-        # update topology attribute
-        self.topology = topology if topology is not None else self.topology
-        # et level consistency with topology
-        level = 2 if self.topology else 1
-        if libvect.Vect_set_open_level(level) != 0:
-            raise OpenError("Invalid access level.")
-        # update the overwrite attribute
-        self.overwrite = overwrite if overwrite is not None else self.overwrite
-        # check if the mode is valid
-        if mode not in ('r', 'rw', 'w'):
-            raise ValueError("Mode not supported. Use one of: 'r', 'rw', 'w'.")
-        # check if the map exist
-        if self.exist() and mode == 'r':
-            openvect = libvect.Vect_open_old2(self.c_mapinfo, self.name,
-                                              self.mapset, layer)
-        # If it is opened in write mode
-        if mode == 'w':
-            openvect = libvect.Vect_open_new(self.c_mapinfo, self.name,
-                                             libvect.WITHOUT_Z)
-        elif mode == 'rw':
-            openvect = libvect.Vect_open_update2(self.c_mapinfo, self.name,
-                                                 self.mapset, layer)
-        # check the C function result.
-        if openvect == 1:
-            self.topology = False
-        elif openvect == 2:
-            self.topology = True
-        else:
-            raise OpenError("Not able to open the map, something wrong.")
-        # initialize the dblinks object
-        self.dblinks = DBlinks(self.c_mapinfo)
-
-    def close(self):
-        if self.isopen():
-            if libvect.Vect_close(self.c_mapinfo) != 0:
-                str_err = 'Error when trying to close the map with Vect_close'
-                raise GrassError(str_err)
-
-    def bbox(self):
-        """Return the BBox of the vecor map
-        """
-        bbox = Bbox()
-        libvect.Vect_get_map_box(self.c_mapinfo, bbox.c_bbox)
-        return bbox
